e0289af79c9f72f0760e17c545887f5576728613
nilton-manjate <ntnmanjate1@gmail.com>
2024-01-18 12:08:40 +0200

    add script to display icon and error message when input is incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-﻿
+﻿"use strict";
+
+const form = document.getElementsByTagName("form")[0];
+const input = document.getElementById("iemail");
+const errorIcon = document.getElementById("error");
+const errorMsg = document.getElementsByTagName("span")[0];
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  if(input.value.includes("@")) {
+    input.style.borderColor = "var(--desatRed)";
+    errorIcon.style.display = "none";
+    errorMsg.style.display = "none";
+  } else {
+    input.style.borderColor = "rgb(249, 100, 100)";
+    errorIcon.style.display = "inline-block";
+    errorMsg.style.display = "block";
+  } 
+});
+
